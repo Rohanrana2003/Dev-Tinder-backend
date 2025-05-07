d2fc31b719406799217e32f68ed38b2474c4f9f5
Rohan rana <rohanrana33042@gmail.com>
2025-05-07 17:13:54 +0530

    Handling route handler with next()

--- src/app.js
+++ src/app.js
@@ -6,15 +6,20 @@ app.get(
   "/user",
   (req, res, next) => {
     next();
+    console.log("handling 1st route handler");
   },
   (req, res, next) => {
     next();
-  },
-  (req, res, next) => {
-    next();
-  },
-  (req, res, next) => {
+    console.log("handling 2nd route handler");
     res.send("Response 2");
+  },
+  (req, res, next) => {
+    next();
+    res.send("Response 3");
+    console.log("handling 3rd route handler");
+  },
+  (req, res, next) => {
+    console.log("handling 4th route handler");
   }
 );
 
