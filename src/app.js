77c851c0ea8a23b1d6b51c38d0c18585dca1341b
Rohan rana <rohanrana33042@gmail.com>
2025-05-09 01:38:57 +0530

    Created an auth middleware

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 const cookieParser = require("cookie-parser");
 var jwt = require("jsonwebtoken");
+const { authUser } = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -71,95 +72,12 @@ app.post("/login", async (req, res) => {
 });
 
 // Profile API
-app.get("/profile", async (req, res) => {
-  const cookies = req.cookies;
-  const { token } = cookies;
-  if (!token) {
-    throw new Error("Invalid Token");
-  }
-
-  const decodedMessage = jwt.verify(token, "Rohan@0274");
-
-  const { _id } = decodedMessage;
-
-  const user = await User.findById(_id);
-  if (!user) {
-    throw new Error("Invalid Credentials");
-  }
-  res.send(user);
-});
-
-// Finding a User by email ID
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.email;
-
+app.get("/profile", authUser, async (req, res) => {
   try {
-    const user = await User.find({ email: userEmail });
-
-    if (user.length === 0) {
-      res.status(404).send("Cannot find user");
-    } else {
-      res.send(user);
-    }
+    const user = req.user;
+    res.send(user);
   } catch (err) {
-    res.status(400).send("Something went wrong");
-  }
-});
-
-// Deleting a User by ID
-app.delete("/user", async (req, res) => {
-  const userId = req.body.id;
-  try {
-    const data = await User.findByIdAndDelete(userId);
-    if (data) {
-      res.send("Deleted Successfully");
-    } else {
-      res.status(404).send("Error in deletion");
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong");
-  }
-});
-
-// Updating a User by ID
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try {
-    const ALLOWED_UPDATES = ["age", "gender", "about", "photoUrl", "skills"];
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-
-    if (!isUpdateAllowed) {
-      throw new Error("Update is not allowed");
-    }
-
-    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
-      runValidators: true,
-    });
-    if (user) {
-      res.send("Updated Successfully");
-    } else {
-      res.status(404).send("Error in Updation");
-    }
-  } catch (err) {
-    res.status(400).send("Updation Error, " + err.message);
-  }
-});
-
-// Fetching all data of Users
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    if (users.length === 0) {
-      res.status(404).send("Cannot find any user");
-    } else {
-      res.send(users);
-    }
-  } catch (err) {
-    res.send(400).send("Something went wrong");
+    res.status(400).send("Error");
   }
 });
 
